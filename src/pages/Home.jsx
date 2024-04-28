f662e744bc968b6b42cd716ab19b37fd24c8de68
Mukunthan A R <mukunthan@geniktech.com>
2024-04-28 16:12:40 +0530

    Passing data to Table Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import InputFilter from "../components/InputFilter";
 import CollegeList from "../components/CollegeList";
 
 const Home = () => {
+  const [filterData, setFilterData] = useState({
+    name: "",
+    email: "",
+    clgName: "",
+    dept: "",
+    cutOff: 0,
+    cast: "",
+    region: "",
+  });
   const handleDataObj = (data) => {
     console.log("parent Data" + data);
+    setFilterData(data);
     console.log(data);
+    console.log(filterData);
   };
   return (
     <div className="my-5">
       <h2 className="text-center font-bold text-xl my-5">TNEA Choice Order </h2>
       <InputFilter sendDataObj={handleDataObj}></InputFilter>
-      <CollegeList></CollegeList>
+      <CollegeList filterData={filterData}></CollegeList>
     </div>
   );
 };
