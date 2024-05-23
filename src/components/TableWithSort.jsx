18f36d3598bdcf13b4201f8a768fa9d2e78e2ca4
Mukunthan <mukunthan.1255@gamil.com>
2024-05-23 20:15:49 +0530

    Refactored UI and Data for testing

--- src/components/TableWithSort.jsx
+++ src/components/TableWithSort.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import ButtonComponent from "./ButtonComponent";
 import { useRecoilState } from "recoil";
 import { resultArray } from "../data/atoms";
+import { FaArrowCircleUp } from "react-icons/fa";
+import { FaArrowCircleDown } from "react-icons/fa";
 
 const TableWithSort = ({ data }) => {
   const [sortedData, setSortedData] = useState(data);
@@ -63,149 +65,207 @@ const TableWithSort = ({ data }) => {
   }, [sortConfig]);
 
   return (
-    <table className="min-w-full  divide-y divide-gray-200 mx-2 sm:mx-3">
-      <thead className="bg-blue-100">
-        <tr>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("sNo")}
-          >
-            S.No
-            {sortConfig && sortConfig.key === "sNo" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("region")}
-          >
-            Region
-            {sortConfig && sortConfig.key === "region" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("collegeCode")}
-          >
-            College Code
-            {sortConfig && sortConfig.key === "collegeCode" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("name")}
-          >
-            Name
-            {sortConfig && sortConfig.key === "name" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("branchCode")}
-          >
-            Branch Code
-            {sortConfig && sortConfig.key === "branchCode" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("branchName")}
-          >
-            Branch Name
-            {sortConfig && sortConfig.key === "branchName" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("oc")}
-          >
-            OC
-            {sortConfig && sortConfig.key === "oc" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("bc")}
-          >
-            BC
-            {sortConfig && sortConfig.key === "bc" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("bcm")}
-          >
-            BCM
-            {sortConfig && sortConfig.key === "bcm" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th
-            className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
-            onClick={() => requestSort("mbc")}
-          >
-            MBC
-            {sortConfig && sortConfig.key === "mbc" && (
-              <span className="ml-1">
-                {sortConfig.direction === "ascending" ? "↑" : "↓"}
-              </span>
-            )}
-          </th>
-          <th className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer">
-            Add
-          </th>
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {sortedData.map((row, rowIndex) => (
-          <tr
-            key={rowIndex}
-            className=" hover:bg-blue-200 focus:bg-red-500 active:bg-green-500 transition-colors duration-100 ease-in-out cursor-pointer"
-          >
-            {Object.entries(row).map(([key, value], cellIndex) => (
-              <td key={cellIndex} className="px-3 py-4 whitespace-nowrap">
-                {value}
-              </td>
-            ))}
-            <td>
-              <ButtonComponent
-                styles="bg-blue-600 p-2 rounded-lg text-white hover:bg-green-400 active:bg-green-500 transition-colors duration-100 ease-in-out cursor-pointer"
-                handleClick={() => {
-                  handleResult(row);
-                }}
-              >
-                Add
-              </ButtonComponent>
-            </td>
+    <div className="flex justify-center">
+      <table className="min-w-full  divide-y divide-gray-200 mx-2 sm:mx-3">
+        <thead className="bg-blue-100">
+          <tr>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("sNo")}
+            >
+              <div className="flex justify-center">
+                S.No
+                {sortConfig && sortConfig.key === "sNo" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleDown size={15} />
+                    ) : (
+                      <FaArrowCircleUp size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("region")}
+            >
+              <div className="flex justify-center">
+                Region
+                {sortConfig && sortConfig.key === "region" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleDown size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer flex items-center"
+              onClick={() => requestSort("collegeCode")}
+            >
+              College Code
+              {sortConfig && sortConfig.key === "collegeCode" && (
+                <span className="ml-1">
+                  {sortConfig.direction === "ascending" ? (
+                    <FaArrowCircleUp size={15} />
+                  ) : (
+                    <FaArrowCircleDown size={15} />
+                  )}
+                </span>
+              )}
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("name")}
+            >
+              <div className="flex items-center">
+                Name
+                {sortConfig && sortConfig.key === "name" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleDown size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className=" px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer "
+              onClick={() => requestSort("branchCode")}
+            >
+              <div className="flex items-center">
+                Branch Code
+                {sortConfig && sortConfig.key === "branchCode" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleUp size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer flex items-center"
+              onClick={() => requestSort("branchName")}
+            >
+              Branch Name
+              {sortConfig && sortConfig.key === "branchName" && (
+                <span className="ml-1">
+                  {sortConfig.direction === "ascending" ? (
+                    <FaArrowCircleUp size={15} />
+                  ) : (
+                    <FaArrowCircleDown size={15} />
+                  )}
+                </span>
+              )}
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("oc")}
+            >
+              <div className="flex items-center">
+                OC
+                {sortConfig && sortConfig.key === "oc" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleDown size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("bc")}
+            >
+              <div className="flex items-center">
+                BC
+                {sortConfig && sortConfig.key === "bc" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleDown size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("bcm")}
+            >
+              <div className="flex items-center">
+                BCM
+                {sortConfig && sortConfig.key === "bcm" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleDown size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th
+              className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer"
+              onClick={() => requestSort("mbc")}
+            >
+              <div className="flex items-center">
+                MBC
+                {sortConfig && sortConfig.key === "mbc" && (
+                  <span className="ml-1">
+                    {sortConfig.direction === "ascending" ? (
+                      <FaArrowCircleUp size={15} />
+                    ) : (
+                      <FaArrowCircleDown size={15} />
+                    )}
+                  </span>
+                )}
+              </div>
+            </th>
+            <th className="px-3 py-3 text-left text-xs font-medium text-gray-900 uppercase tracking-wider cursor-pointer">
+              Add
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {sortedData.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              className=" hover:bg-blue-200 focus:bg-red-500 active:bg-green-500 transition-colors duration-100 ease-in-out cursor-pointer"
+            >
+              {Object.entries(row).map(([key, value], cellIndex) => (
+                <td key={cellIndex} className="px-3 py-4 whitespace-nowrap">
+                  {value}
+                </td>
+              ))}
+              <td>
+                <ButtonComponent
+                  styles="bg-blue-600 p-2 rounded-lg text-white hover:bg-green-400 active:bg-green-500 transition-colors duration-100 ease-in-out cursor-pointer"
+                  handleClick={() => {
+                    handleResult(row);
+                  }}
+                >
+                  Add
+                </ButtonComponent>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
