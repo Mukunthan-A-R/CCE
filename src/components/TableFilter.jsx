6d8f4e42663650cae548ecca9b8b64274938eb04
Thejvanth <thejvanth08@gmail.com>
2024-07-25 22:52:33 +0530

    Fix table filter

--- src/components/TableFilter.jsx
+++ src/components/TableFilter.jsx
@@ -1,20 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import TableValues from '../data/DataChennai';
-import InputComponent from './InputComponent';
-import ButtonComponent from './ButtonComponent';
-import InputRegion from './InputRegion';
-import InputDept from './InputDept';
-import TableWithSort from './TableWithSort';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
+import React, { useEffect, useState } from "react";
+import TableValues from "../data/DataChennai";
+import InputComponent from "./InputComponent";
+import ButtonComponent from "./ButtonComponent";
+import InputRegion from "./InputRegion";
+import InputDept from "./InputDept";
+import TableWithSort from "./TableWithSort";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/20/solid";
 import { userData } from "../data/atoms";
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue } from "recoil";
 
 const TableFilter = () => {
-  const { community }= useRecoilValue(userData);
-  // console.log("com", community);  
-  
+  const { community } = useRecoilValue(userData);
+
   const [data, setData] = useState([...TableValues]);
-  console.log("--->", data[0]);
   const [filter, setFilter] = useState({
     cutOffStart: 200,
     cutOffEnd: 0,
@@ -22,34 +20,113 @@ const TableFilter = () => {
     region: "",
     dept: [],
   });
-  const [errors, SetErrors] = useState({});
+  const [errors, setErrors] = useState({});
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageWindowStart, setPageWindowStart] = useState(1); // New state for pagination window
+  const [pageWindowStart, setPageWindowStart] = useState(1);
   const pageSize = 50;
 
+  const handleDataCutOffSt = (value) => {
+    let newErrors = {};
 
-  // Filter logic
+    if (value > 0 && value <= 200) {
+      setFilter((prevFilter) => ({
+        ...prevFilter,
+        cutOffStart: parseInt(value),
+      }));
+      newErrors.cutOffStart = "";
+    } else if (value === "") {
+      setFilter((prevFilter) => ({ ...prevFilter, cutOffStart: 200 }));
+    } else if (value > 200) {
+      newErrors.cutOffStart =
+        "Cut off value starting should be between 1 and 200";
+    } else if (value < 0) {
+      newErrors.cutOffStart = "Cut off value should not be less than 0";
+    }
+
+    setErrors((prevErrors) => ({ ...prevErrors, ...newErrors }));
+  };
+
+  const handleDataCutOffEnd = (value) => {
+    let newErrors = {};
+
+    if (value > 200) {
+      newErrors.cutOffEnd = "The cut off value should be between 1 and 200";
+    } else if (value < 0) {
+      newErrors.cutOffEnd = "The cut off value should be greater than 0";
+    } else if (value > 0 && value <= 200) {
+      setFilter((prevFilter) => ({
+        ...prevFilter,
+        cutOffEnd: parseInt(value),
+      }));
+      newErrors.cutOffEnd = "";
+    } else if (value === "") {
+      setFilter((prevFilter) => ({ ...prevFilter, cutOffEnd: 0 }));
+    }
+
+    setErrors((prevErrors) => ({ ...prevErrors, ...newErrors }));
+  };
+
+  const handleDataCollegeCode = (value) => {
+    const intValue = parseInt(value);
+    let newErrors = {};
+
+    if (value === "") {
+      setFilter((prevFilter) => ({ ...prevFilter, collegeCode: 0 }));
+      newErrors.collegeCode = "";
+    } else if (isNaN(intValue)) {
+      newErrors.collegeCode = "College Code must be a number";
+    } else if (intValue <= 0 || intValue > 3000) {
+      newErrors.collegeCode = "The college code should be between 1 and 3000";
+    } else {
+      setFilter((prevFilter) => ({ ...prevFilter, collegeCode: intValue }));
+      newErrors.collegeCode = "";
+    }
+
+    setErrors((prevErrors) => ({ ...prevErrors, ...newErrors }));
+  };
+
+  const handleDataRegion = (e) => {
+    setFilter((prevFilter) => ({ ...prevFilter, region: e.target.value }));
+  };
+
+  const handleDataDept = (selectedValues) => {
+    setFilter((prevFilter) => ({ ...prevFilter, dept: selectedValues }));
+  };
+
   const handleSubmit = () => {
-    const hasErrors = Object.values(errors).some(error => error.trim() !== '');
+    const hasErrors = Object.values(errors).some(
+      (error) => error.trim() !== ""
+    );
     if (hasErrors) {
-      console.log('Form Contains errors');
+      console.log("Form contains errors");
       return;
     }
 
-    let filteredData = data.filter(value => value.oc <= filter.cutOffStart && value.oc >= filter.cutOffEnd &&
-      (filter.region === "" || value.region.toLowerCase() === filter.region.toLowerCase())
+    // Initial filter based on cutOffStart, cutOffEnd, and region
+    let filteredData = data.filter(
+      (value) =>
+        value.oc <= filter.cutOffStart &&
+        value.oc >= filter.cutOffEnd &&
+        (filter.region === "" ||
+          (typeof filter.region === "string" &&
+            value.region.toLowerCase() === filter.region.toLowerCase()))
     );
-    
-    // college code
+
+    // Additional filter based on collegeCode
     if (filter.collegeCode !== 0) {
-      filteredData = filteredData.filter(value => filter.collegeCode === parseInt(value.collegeCode));
+      filteredData = filteredData.filter(
+        (value) => filter.collegeCode === parseInt(value.collegeCode)
+      );
     }
 
-    // departments
+    // Additional filter based on department
     if (filter.dept.length !== 0) {
-      filteredData = filteredData.filter(value => filter.dept.includes(value.branchCode));
+      filteredData = filteredData.filter((value) =>
+        filter.dept.includes(value.branchCode)
+      );
     }
 
+    // Set the filtered data to state
     setData(filteredData);
     setCurrentPage(1); // Reset to the first page after filtering
     setPageWindowStart(1); // Reset the pagination window start
@@ -57,33 +134,31 @@ const TableFilter = () => {
 
   useEffect(() => {
     let communityFilteredData;
-    if (community == "oc") {
-      communityFilteredData = data.map((data) => ({
+    if (community === "oc") {
+      communityFilteredData = TableValues.map((data) => ({
         sNo: data.sNo,
         region: data.region,
         collegeCode: data.collegeCode,
         name: data.name,
         branchCode: data.branchCode,
         branchName: data.branchName,
-        oc: data.oc
+        oc: data.oc,
       }));
     } else {
-      communityFilteredData = data.map((data) => ({
+      communityFilteredData = TableValues.map((data) => ({
         sNo: data.sNo,
         region: data.region,
         collegeCode: data.collegeCode,
         name: data.name,
         branchCode: data.branchCode,
         branchName: data.branchName,
-        oc: data.oc,  
-        [community]: data[community], // Dynamic key based on the community
+        oc: data.oc,
+        [community]: data[community],
       }));
     }
     setData(communityFilteredData);
-    console.log("data", communityFilteredData[0]);
-  }, []);
+  }, [community]);
 
-  // Pagination logic
   const totalPages = Math.ceil(data.length / pageSize);
   const startIndex = (currentPage - 1) * pageSize;
   const paginatedData = data.slice(startIndex, startIndex + pageSize);
@@ -114,7 +189,11 @@ const TableFilter = () => {
 
   const getPageNumbers = () => {
     const pageNumbers = [];
-    for (let i = pageWindowStart; i < pageWindowStart + 3 && i <= totalPages; i++) {
+    for (
+      let i = pageWindowStart;
+      i < pageWindowStart + 3 && i <= totalPages;
+      i++
+    ) {
       pageNumbers.push(i);
     }
     return pageNumbers;
@@ -172,15 +251,23 @@ const TableFilter = () => {
         <button
           onClick={handlePreviousPage}
           disabled={currentPage === 1}
-          className={`mx-1 p-2 rounded ${currentPage === 1 ? 'bg-gray-200 text-gray-400' : 'bg-gray-200 text-black'}`}
+          className={`mx-1 p-2 rounded ${
+            currentPage === 1
+              ? "bg-gray-200 text-gray-400"
+              : "bg-gray-200 text-black"
+          }`}
         >
           <ChevronLeftIcon className="w-6 h-6" />
         </button>
-        {getPageNumbers().map(page => (
+        {getPageNumbers().map((page) => (
           <button
             key={page}
             onClick={() => handlePageChange(page)}
-            className={`mx-1 px-3 py-1 rounded ${currentPage === page ? 'bg-blue-700 text-white' : 'bg-gray-200 text-black'}`}
+            className={`mx-1 px-3 py-1 rounded ${
+              currentPage === page
+                ? "bg-blue-700 text-white"
+                : "bg-gray-200 text-black"
+            }`}
           >
             {page}
           </button>
@@ -188,7 +275,11 @@ const TableFilter = () => {
         <button
           onClick={handleNextPage}
           disabled={currentPage === totalPages}
-          className={`mx-1 p-2 rounded ${currentPage === totalPages ? 'bg-gray-200 text-gray-400' : 'bg-gray-200 text-black'}`}
+          className={`mx-1 p-2 rounded ${
+            currentPage === totalPages
+              ? "bg-gray-200 text-gray-400"
+              : "bg-gray-200 text-black"
+          }`}
         >
           <ChevronRightIcon className="w-6 h-6" />
         </button>
