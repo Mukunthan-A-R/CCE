bfe67b0123e352b943cbda43b22e60967f130157
rahulj1011 <rahuljr2022@gmail.com>
2024-07-25 13:33:51 +0530

    Added validations

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const InputComponent = ({ label, type = "text", styles, sendData }) => {
+const InputComponent = ({ label, type = "text", styles, sendData,error}) => {
   const handleChange = (event) => {
     sendData(event.target.value);
   };
@@ -17,7 +17,7 @@ const InputComponent = ({ label, type = "text", styles, sendData }) => {
         id={label}
         className="w-full border bg-[#deebfa] border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:border-blue-500"
         type={type}
-        placeholder={placeholder}
+        
       />
       {error && <p className="text-red-500">{error}</p>}
     </div>
