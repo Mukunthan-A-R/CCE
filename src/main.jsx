ec9a4ced99f067e2f34f71be2315914cfbb9ece6
Thejvanth <thejvanth08@gmail.com>
2024-07-25 13:39:11 +0530

    Add ReactGA for analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes.jsx";
 import { RecoilRoot } from "recoil";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-9LSP8TEJB9");
+ReactGA.send({ hitType: "pageview", page: "window.location.pathname" });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
