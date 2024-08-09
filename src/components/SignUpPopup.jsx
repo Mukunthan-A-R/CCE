a19990d3d0fb3d3723d777e70a43021c6dbd690f
Mukunthan A R <mukunthan.1255@gmail.com>
2024-08-09 15:08:23 +0530

    Expert login Error

--- src/components/SignUpPopup.jsx
+++ src/components/SignUpPopup.jsx
@@ -7,6 +7,7 @@ const SignUpPopup = ({ onAccept, onCancel , setExpert}) => {
         name:"",
         password:""
     })
+    const [error,setError] = useState(false); 
     const navigate = useNavigate();
 
     const handleExpert = () => {
@@ -14,7 +15,11 @@ const SignUpPopup = ({ onAccept, onCancel , setExpert}) => {
         if(expertData.name === "sit2024" && expertData.password === "tnea@2024") {
             console.log("Correct Data");  
             navigate("/home");
-    }}
+        }
+        else{
+            setError(true)
+        }
+}
         
 
   return (
@@ -52,6 +57,11 @@ const SignUpPopup = ({ onAccept, onCancel , setExpert}) => {
                         </div>
                         <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
                     </div> */}
+                    {
+                        error && 
+                        <p className="text-red-500 font-medium">The Id or password field is incorrect</p>
+
+                    }
                     <button type="button" onClick={handleExpert}
                      class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                     {/* <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
